11b8a36d987e8a82ad1ffc52bd8fa56ca885ed99
adarsh12327 <kumaradarshpatel30@gmail.com>
2026-05-19 01:17:15 +0530

    server.js

--- server.js
+++ server.js
@@ -7,11 +7,78 @@ require("firebase-admin");
 const app =
 express();
 
+const firebaseKey =
+JSON.parse(
+process.env
+.FIREBASE_KEY);
+
+admin.initializeApp({
+credential:
+admin.credential
+.cert(firebaseKey),
+
+databaseURL:
+"https://kap-rewards-afb54-default-rtdb.firebaseio.com"
+});
+
+const db =
+admin.database();
+
 app.get("/",
 (req, res) => {
 
 res.send(
 "KAP Rewards Running");
+});
+
+app.get(
+"/postback",
+async (
+req,
+res) => {
+
+try {
+
+const uid =
+req.query.uid;
+
+const reward =
+parseInt(
+req.query.reward
+|| 0);
+
+if(!uid ||
+!reward) {
+
+return res.send(
+"Missing uid or reward");
+}
+
+const userRef =
+db.ref(
+`users/${uid}/coins`);
+
+const snap =
+await userRef.once(
+"value");
+
+const oldCoins =
+parseInt(
+snap.val()
+|| 0);
+
+await userRef.set(
+oldCoins
++ reward);
+
+res.send(
+"Reward Added");
+
+} catch(e) {
+
+res.send(
+e.toString());
+}
 });
 
 app.listen(
